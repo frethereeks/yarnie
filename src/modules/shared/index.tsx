891b5d060f1237d6681a0355452f4c8722b27a9b
Fredericks <Adefredy1@gmail.com>
2025-07-16 20:36:15 +0100

    Fix: Remove the Public Search Component (not needed for this project)

--- src/modules/shared/index.tsx
+++ src/modules/shared/index.tsx
@@ -3,7 +3,7 @@ import Footer from "@/modules/shared/components/Footer"
 import HappyClientSection from "@/modules/shared/components/HappyClientSection"
 import ProductCard from "@/modules/shared/components/ProductCard"
 import SlugAddBtn from "@/modules/shared/components/SlugAddBtn"
-import SearchInput from "@/modules/shared/components/SearchInput"
+// import SearchInput from "@/modules/shared/components/SearchInput"
 
 export {
     Header,
@@ -11,5 +11,5 @@ export {
     HappyClientSection,
     ProductCard,
     SlugAddBtn,
-    SearchInput,
+    // SearchInput,
 }
